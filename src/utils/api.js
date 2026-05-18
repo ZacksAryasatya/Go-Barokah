9378accb041c3caa7e8f03749722d37470d70d83
Zaky <syafazaqi02@gmail.com>
2026-05-18 20:10:52 +0700

    fix: add withCredentials for cross-origin requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,7 @@ export const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true',
